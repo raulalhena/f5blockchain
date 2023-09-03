ec9a1ce842e06b4f3732da02e06f35724d7f7b00
Raul <raul.alhena@gmail.com>
2023-09-03 15:30:06 +0200

    connected to metamask with ethereum injected object

--- frontend/src/pages/Dapps.tsx
+++ frontend/src/pages/Dapps.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import {useState} from 'react'
 import DenseAppBar from '../components/Navbar'
+import { Box, Typography } from '@mui/material'
+
+
+const connectionButton = {
+  width: '200px', 
+  height: '50px', 
+  fontSize: '20px', 
+  color: '#fff', 
+  backgroundColor: 'purple', 
+  borderRadius: '5px' 
+}
 
 function Dapps() {
+  const [wallet, setWallet] = useState();
+
+  const requestAccount = async () => {
+    if(window.ethereum) {
+      console.log('detected');
+      try{
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts'
+        });
+        setWallet(accounts)
+      } catch(error) {
+        console.log(error);
+      }
+    } else {
+      console.log('Metamask not detected')
+    }
+  }
+
   return (
     <>
-        <DenseAppBar />
-        <div>Dapps</div>
+      <DenseAppBar />
+      <main style={{ height: '100vh' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', border: '1px solid blue' }} >
+          <button style={connectionButton} onClick={requestAccount}>Connect Wallet</button>
+          <Typography sx={{ fontSize: '30px' }}><strong>Wallet:</strong></Typography>
+          <Typography sx={{ fontSize: '25px' }}>{wallet}</Typography>
+        </Box>
+      </main>
     </>
   )
 }
